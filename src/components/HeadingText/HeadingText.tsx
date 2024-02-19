c3167662d6ef779eb43b88f5a298ce7ddd7975bd
anujkumarchoudhary <timetechapps1@gmail.com>
2024-02-19 02:40:17 -0800

    changes by anuj

--- src/components/HeadingText/HeadingText.tsx
+++ src/components/HeadingText/HeadingText.tsx
@@ -12,11 +12,14 @@ export interface IHeadingText {
     IsAction?: boolean;
     name2?: string;
     handleClick2?: any;
+    handleClick3?:any;
     IsName2?: boolean;
+    IsName3?: boolean;
+    name3?: any
     IsSearchBox?: boolean
     setQuery?: any
 }
-const HeadingText = ({ heading, name, name2, IsName2, handleClick, IsAction, handleClick2, IsSearchBox, setQuery }: IHeadingText) => {
+const HeadingText = ({ heading, name, name2, name3, IsName3, handleClick3, IsName2, handleClick, IsAction, handleClick2, IsSearchBox, setQuery }: IHeadingText) => {
     return (
         <Grid className={styles.headingTextContainer}>
             <Typography variant='h4' fontWeight={500} fontSize={25}>{heading}</Typography>
@@ -27,6 +30,7 @@ const HeadingText = ({ heading, name, name2, IsName2, handleClick, IsAction, han
 
                     <CommonButton name={name} onClick={handleClick} />
                     {IsName2 ? <CommonButton name={name2} onClick={handleClick2} /> : ""}
+                    {IsName3 ? <CommonButton name={name3} onClick={handleClick3} /> : ""}
                 </Grid> :
                 ""}
         </Grid>

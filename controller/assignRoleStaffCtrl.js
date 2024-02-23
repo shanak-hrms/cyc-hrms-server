ed3fd38e75b4e731d3f6f2231b5d76140e4dec09
Narendra-LionSafariTech <nrsingh037@gmail.com>
2024-02-23 18:36:53 +0530

    role and department

--- controller/assignRoleStaffCtrl.js
+++ controller/assignRoleStaffCtrl.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const User = require('../model/user');
 
+
+const compareRoles=(newRole, currentRole)=> {
+    const rolesHierarchy = {
+      "EMPLOYEE": 0,
+      "MANAGER": 1,
+      "DIRECTOR": 2,
+    };
+    return (rolesHierarchy[newRole] || 0) - (rolesHierarchy[currentRole] || 0);
+  }
+
 exports.assignEmployeeAsLineManager = async (req, res) => {
     try {
         const { employeeId } = req.params;
-        const { role } = req.user
+        const { role,} = req.user
+        const {newRole,newDepartment}=req.body
+        console.log(newRole,newDepartment)
         if (role !== "HR") {
             throw new Error("Only HR is allowed to assign Employee as a Manager.");
         }
@@ -12,7 +24,16 @@ exports.assignEmployeeAsLineManager = async (req, res) => {
         if (!employee) {
             return res.status(404).json({ error: "User not found" });
         }
-        employee.role = "MANAGER";
+        if(newRole){
+            if (compareRoles(newRole, employee.role) < 0) {
+                throw new Error("Demotion is not allowed");
+              }
+            employee.role =newRole;
+        }
+        if(newDepartment){
+            employee.department =newDepartment;
+        }
+
         await employee.save();
         res.status(200).json({ message: "Employee role updated successfully" });
     } catch (error) {

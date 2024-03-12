6aac9cb6b7754169c626e5c32a272313b67df772
shanak-hrms <163140124+shanak-hrms@users.noreply.github.com>
2024-03-12 18:35:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.use((req, res, next) => {
     });
 });
 
-
-
-module.exports = app;
+const PORT = process.env.PORT || 3080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
